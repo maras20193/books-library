621adbdb1e80640e2d2a7dd728fd59d189e01f84
Łukasz Markowski <lucas20193@gmail.com>
2021-05-19 14:47:03 +0200

    add Create component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { blue, red } from '@material-ui/core/colors';
+
+import Create from './pages/Create';
+
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            {/* <Route exact path="/">
+            </Route> */}
+            <Route path="/">
+              <Create/>
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+
     </div>
   );
 }
---
+++ src/pages/Create.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+
+import {
+  Button, 
+  Container, 
+  FormControl, 
+  FormControlLabel, 
+  FormLabel, 
+  Radio, 
+  RadioGroup, 
+  TextField, 
+  Typography, 
+  withWidth
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+  },
+  radio: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+})
+
+
+const Create = () => {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [category, setCategory] = useState('home')
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (!title) setTitleError(true);
+    if (!details) setDetailsError(true);
+
+    if( title && details) {
+      const note = {
+        title,
+        details,
+      };
+      console.log(note);
+    }
+  }
+
+    return (
+        <Container>
+          <Typography 
+            variant='h4'
+            component='h2'
+            color="textSecondary"
+            gutterBottom
+          >
+            Add new task
+          </Typography>
+
+          <form 
+            onSubmit={handleSubmit}
+            action="submit"
+            noValidate
+            autoComplete='off'>
+
+          <TextField 
+            className={classes.field}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            label="Note Title" 
+            variant="outlined"
+            color="primary"
+            fullWidth
+            required
+            error={titleError}
+            />
+          <TextField
+            className={classes.field}
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+            label="Details" 
+            variant="outlined"
+            color="primary"
+            fullWidth
+            multiline
+            rows={4}
+            required
+            error={detailsError}
+            />
+
+          <FormControl className={classes.field}>
+            <FormLabel>Note Category</FormLabel>
+            <RadioGroup className={classes.radio} value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel value='home' control={<Radio/>} label="Home" />
+            <FormControlLabel value='work' control={<Radio/>} label="Work" />
+            <FormControlLabel value='health' control={<Radio/>} label="Health" />
+            <FormControlLabel value='books' control={<Radio/>} label="Books" />
+          </RadioGroup>
+          </FormControl>
+          
+
+          <Button
+            type="submit"
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            size="large"
+            endIcon={<SendIcon/>}
+            >
+            Submit
+          </Button>
+
+          </form>
+
+        </Container>
+    );
+}
+
+export default Create
